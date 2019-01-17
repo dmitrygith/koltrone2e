934c4c27eae6ab052a2c0d5a2552bf87090f0308
dmitrygith <dmitryiv52@gmail.com>
2019-01-17 16:01:52 +0300

    Merged master fixed conflict.

--- src/main/java/com/reloadbet/elements/DropDownAccMenu.java
+++ src/main/java/com/reloadbet/elements/DropDownAccMenu.java
@@ -14,8 +14,6 @@ public class DropDownAccMenu {
     public String balanceTitleMoneyHomepage = ".osg-user-container__real-money-title";
     public String moneySum = ".osg-user-container__real-money-value .osg-money";
     public String menuContainer = ".osg-dropdown__content";
-
-
 }
 
 
